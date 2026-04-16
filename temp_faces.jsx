54130b949df5d5c825f72b58c80409734cc3353c
Nuwangi <webinnt@gmail.com>
2026-04-16 15:04:53 +0530

    talk to an expert section creation temp faces file

---
+++ temp_faces.jsx
@@ -0,0 +1,196 @@
+              <div className="faces-row faces-row-2">
+                {/* Second row - 30 images with different starting order */}
+                <div className="face-item">
+                  <img src={`${process.env.PUBLIC_URL}/images/people5.png`} alt="Person 5" className="face-image" />
+                </div>
+                <div className="face-item">
+                  <img src={`${process.env.PUBLIC_URL}/images/people6.jpg`} alt="Person 6" className="face-image" />
+                </div>
+                <div className="face-item">
+                  <img src={`${process.env.PUBLIC_URL}/images/people7.jpg`} alt="Person 7" className="face-image" />
+                </div>
+                <div className="face-item">
+                  <img src={`${process.env.PUBLIC_URL}/images/people8.jpg`} alt="Person 8" className="face-image" />
+                </div>
+                <div className="face-item">
+                  <img src={`${process.env.PUBLIC_URL}/images/people9.jpg`} alt="Person 9" className="face-image" />
+                </div>
+                <div className="face-item">
+                  <img src={`${process.env.PUBLIC_URL}/images/people10.jpg`} alt="Person 10" className="face-image" />
+                </div>
+                <div className="face-item">
+                  <img src={`${process.env.PUBLIC_URL}/images/people1.jpg`} alt="Person 1" className="face-image" />
+                </div>
+                <div className="face-item">
+                  <img src={`${process.env.PUBLIC_URL}/images/people2.jpg`} alt="Person 2" className="face-image" />
+                </div>
+                <div className="face-item">
+                  <img src={`${process.env.PUBLIC_URL}/images/people3.jpg`} alt="Person 3" className="face-image" />
+                </div>
+                <div className="face-item">
+                  <img src={`${process.env.PUBLIC_URL}/images/people4.jpg`} alt="Person 4" className="face-image" />
+                </div>
+                <div className="face-item">
+                  <img src={`${process.env.PUBLIC_URL}/images/people5.png`} alt="Person 5" className="face-image" />
+                </div>
+                <div className="face-item">
+                  <img src={`${process.env.PUBLIC_URL}/images/people6.jpg`} alt="Person 6" className="face-image" />
+                </div>
+                <div className="face-item">
+                  <img src={`${process.env.PUBLIC_URL}/images/people7.jpg`} alt="Person 7" className="face-image" />
+                </div>
+                <div className="face-item">
+                  <img src={`${process.env.PUBLIC_URL}/images/people8.jpg`} alt="Person 8" className="face-image" />
+                </div>
+                <div className="face-item">
+                  <img src={`${process.env.PUBLIC_URL}/images/people9.jpg`} alt="Person 9" className="face-image" />
+                </div>
+                <div className="face-item">
+                  <img src={`${process.env.PUBLIC_URL}/images/people10.jpg`} alt="Person 10" className="face-image" />
+                </div>
+                <div className="face-item">
+                  <img src={`${process.env.PUBLIC_URL}/images/people1.jpg`} alt="Person 1" className="face-image" />
+                </div>
+                <div className="face-item">
+                  <img src={`${process.env.PUBLIC_URL}/images/people2.jpg`} alt="Person 2" className="face-image" />
+                </div>
+                <div className="face-item">
+                  <img src={`${process.env.PUBLIC_URL}/images/people3.jpg`} alt="Person 3" className="face-image" />
+                </div>
+                <div className="face-item">
+                  <img src={`${process.env.PUBLIC_URL}/images/people4.jpg`} alt="Person 4" className="face-image" />
+                </div>
+                <div className="face-item">
+                  <img src={`${process.env.PUBLIC_URL}/images/people5.png`} alt="Person 5" className="face-image" />
+                </div>
+                <div className="face-item">
+                  <img src={`${process.env.PUBLIC_URL}/images/people6.jpg`} alt="Person 6" className="face-image" />
+                </div>
+                <div className="face-item">
+                  <img src={`${process.env.PUBLIC_URL}/images/people7.jpg`} alt="Person 7" className="face-image" />
+                </div>
+                <div className="face-item">
+                  <img src={`${process.env.PUBLIC_URL}/images/people8.jpg`} alt="Person 8" className="face-image" />
+                </div>
+                <div className="face-item">
+                  <img src={`${process.env.PUBLIC_URL}/images/people9.jpg`} alt="Person 9" className="face-image" />
+                </div>
+                <div className="face-item">
+                  <img src={`${process.env.PUBLIC_URL}/images/people10.jpg`} alt="Person 10" className="face-image" />
+                </div>
+                <div className="face-item">
+                  <img src={`${process.env.PUBLIC_URL}/images/people1.jpg`} alt="Person 1" className="face-image" />
+                </div>
+                <div className="face-item">
+                  <img src={`${process.env.PUBLIC_URL}/images/people2.jpg`} alt="Person 2" className="face-image" />
+                </div>
+                <div className="face-item">
+                  <img src={`${process.env.PUBLIC_URL}/images/people3.jpg`} alt="Person 3" className="face-image" />
+                </div>
+                <div className="face-item">
+                  <img src={`${process.env.PUBLIC_URL}/images/people4.jpg`} alt="Person 4" className="face-image" />
+                </div>
+              </div>
+              
+              <div className="faces-row faces-row-3">
+                {/* Third row - 30 images with another different starting order */}
+                <div className="face-item">
+                  <img src={`${process.env.PUBLIC_URL}/images/people8.jpg`} alt="Person 8" className="face-image" />
+                </div>
+                <div className="face-item">
+                  <img src={`${process.env.PUBLIC_URL}/images/people9.jpg`} alt="Person 9" className="face-image" />
+                </div>
+                <div className="face-item">
+                  <img src={`${process.env.PUBLIC_URL}/images/people10.jpg`} alt="Person 10" className="face-image" />
+                </div>
+                <div className="face-item">
+                  <img src={`${process.env.PUBLIC_URL}/images/people1.jpg`} alt="Person 1" className="face-image" />
+                </div>
+                <div className="face-item">
+                  <img src={`${process.env.PUBLIC_URL}/images/people2.jpg`} alt="Person 2" className="face-image" />
+                </div>
+                <div className="face-item">
+                  <img src={`${process.env.PUBLIC_URL}/images/people3.jpg`} alt="Person 3" className="face-image" />
+                </div>
+                <div className="face-item">
+                  <img src={`${process.env.PUBLIC_URL}/images/people4.jpg`} alt="Person 4" className="face-image" />
+                </div>
+                <div className="face-item">
+                  <img src={`${process.env.PUBLIC_URL}/images/people5.png`} alt="Person 5" className="face-image" />
+                </div>
+                <div className="face-item">
+                  <img src={`${process.env.PUBLIC_URL}/images/people6.jpg`} alt="Person 6" className="face-image" />
+                </div>
+                <div className="face-item">
+                  <img src={`${process.env.PUBLIC_URL}/images/people7.jpg`} alt="Person 7" className="face-image" />
+                </div>
+                <div className="face-item">
+                  <img src={`${process.env.PUBLIC_URL}/images/people8.jpg`} alt="Person 8" className="face-image" />
+                </div>
+                <div className="face-item">
+                  <img src={`${process.env.PUBLIC_URL}/images/people9.jpg`} alt="Person 9" className="face-image" />
+                </div>
+                <div className="face-item">
+                  <img src={`${process.env.PUBLIC_URL}/images/people10.jpg`} alt="Person 10" className="face-image" />
+                </div>
+                <div className="face-item">
+                  <img src={`${process.env.PUBLIC_URL}/images/people1.jpg`} alt="Person 1" className="face-image" />
+                </div>
+                <div className="face-item">
+                  <img src={`${process.env.PUBLIC_URL}/images/people2.jpg`} alt="Person 2" className="face-image" />
+                </div>
+                <div className="face-item">
+                  <img src={`${process.env.PUBLIC_URL}/images/people3.jpg`} alt="Person 3" className="face-image" />
+                </div>
+                <div className="face-item">
+                  <img src={`${process.env.PUBLIC_URL}/images/people4.jpg`} alt="Person 4" className="face-image" />
+                </div>
+                <div className="face-item">
+                  <img src={`${process.env.PUBLIC_URL}/images/people5.png`} alt="Person 5" className="face-image" />
+                </div>
+                <div className="face-item">
+                  <img src={`${process.env.PUBLIC_URL}/images/people6.jpg`} alt="Person 6" className="face-image" />
+                </div>
+                <div className="face-item">
+                  <img src={`${process.env.PUBLIC_URL}/images/people7.jpg`} alt="Person 7" className="face-image" />
+                </div>
+                <div className="face-item">
+                  <img src={`${process.env.PUBLIC_URL}/images/people8.jpg`} alt="Person 8" className="face-image" />
+                </div>
+                <div className="face-item">
+                  <img src={`${process.env.PUBLIC_URL}/images/people9.jpg`} alt="Person 9" className="face-image" />
+                </div>
+                <div className="face-item">
+                  <img src={`${process.env.PUBLIC_URL}/images/people10.jpg`} alt="Person 10" className="face-image" />
+                </div>
+                <div className="face-item">
+                  <img src={`${process.env.PUBLIC_URL}/images/people1.jpg`} alt="Person 1" className="face-image" />
+                </div>
+                <div className="face-item">
+                  <img src={`${process.env.PUBLIC_URL}/images/people2.jpg`} alt="Person 2" className="face-image" />
+                </div>
+                <div className="face-item">
+                  <img src={`${process.env.PUBLIC_URL}/images/people3.jpg`} alt="Person 3" className="face-image" />
+                </div>
+                <div className="face-item">
+                  <img src={`${process.env.PUBLIC_URL}/images/people4.jpg`} alt="Person 4" className="face-image" />
+                </div>
+                <div className="face-item">
+                  <img src={`${process.env.PUBLIC_URL}/images/people5.png`} alt="Person 5" className="face-image" />
+                </div>
+                <div className="face-item">
+                  <img src={`${process.env.PUBLIC_URL}/images/people6.jpg`} alt="Person 6" className="face-image" />
+                </div>
+                <div className="face-item">
+                  <img src={`${process.env.PUBLIC_URL}/images/people7.jpg`} alt="Person 7" className="face-image" />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default HomePage;
