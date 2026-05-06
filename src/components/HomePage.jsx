fb052149d0101c15fb5c24a81051a91ffa091f3b
Nuwangi <webinnt@gmail.com>
2026-05-06 17:09:46 +0530

    video section not playing bug fix_5

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -429,8 +429,10 @@ const HomePage = () => {
                controls={false}
                webkit-playsinline="true"
                x-webkit-airplay="allow"
+               crossOrigin="anonymous"
              >
-               <source src="/Video/video.mp4" type="video/mp4" />
+               <source src="https://branchworks-video.s3.eu-north-1.amazonaws.com/Branchwork+Website_V03+(For+Web_MP4).mp4" type="video/mp4" />
+               <source src={`${process.env.PUBLIC_URL}/Video/video.mp4`} type="video/mp4" />
                Your browser does not support the video tag.
              </video>
            </div>
